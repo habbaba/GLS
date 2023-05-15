f9e587fb1e63e43b829188cc7f8f11c62e4add8c
celaltas <60509470+celaltas@users.noreply.github.com>
2023-05-15 21:56:40 +0300

    related fields joined stock_move_line

--- glsgroup-main/models/weight_measurement.py
+++ glsgroup-main/models/weight_measurement.py
@@ -24,9 +24,9 @@ class WeightMeasurement(models.Model):
         ]
 
     gls_stock_id = fields.Many2one('gls.stock', compute="compute_gls_stock_id", store=True)
-    partner_id = fields.Many2one('res.partner', related='gls_stock_id.partner_id', string='Tedarikçi')
-    product_id = fields.Many2one('product.product', related='gls_stock_id.product_id', string='Ürün')
-    product_qty = fields.Float(related='gls_stock_id.stock_move_id.product_qty', string='Adet')
+    partner_id = fields.Many2one('res.partner', related='stock_move_line_id.move_id.picking_id.partner_id', string='Tedarikçi')
+    product_id = fields.Many2one('product.product', related='stock_move_line_id.move_id.product_id', string='Ürün')
+    product_qty = fields.Float(related='stock_move_line_id.move_id.product_uom_qty', string='Adet')
     line_ids = fields.One2many('weight.measurement.line', 'weight_measurement_id')
     gross_weight = fields.Float()
     net_weight = fields.Float()
@@ -35,9 +35,9 @@ class WeightMeasurement(models.Model):
 
     def action_create_pallet_lines(self):
         self.ensure_one()
-        if self.gls_stock_id and not self.line_ids:
+        if self.stock_move_line_id and not self.line_ids:
             self.line_ids = [(0, 0, {'name': f'Palet {i}', })
-                             for i in range(1, int(self.gls_stock_id.stock_move_id.product_qty) + 1)]
+                             for i in range(1, int(self.product_qty) + 1)]
 
     @api.constrains('line_ids')
     def _constraint_line_ids(self):
