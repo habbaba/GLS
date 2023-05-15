3a49d5cfdead8191c4684430d9c6916ec019794e
celaltas <60509470+celaltas@users.noreply.github.com>
2023-05-15 21:33:14 +0300

    changed domain for product movements

--- glsgroup-main/models/weight_measurement.py
+++ glsgroup-main/models/weight_measurement.py
@@ -1,4 +1,4 @@
-from odoo import fields, models,api
+from odoo import fields, models, api
 
 
 class WeightMeasurement(models.Model):
@@ -6,22 +6,39 @@ class WeightMeasurement(models.Model):
     _rec_name = "gls_stock_id"
 
 
+    @api.model
+    def _get_move_line_domain(self):
+        self.env.cr.execute("""
+                                select sml.id from stock_move_line sml
+                                join stock_picking sp on sp.id = sml.picking_id
+                                join stock_picking_type spt on spt.id = sp.picking_type_id
+                                where sml.lot_id is not NULL
+                                and sml.location_dest_id=10
+                                and spt.id=1
+                                """)  
+        result = self.env.cr.fetchall()
+        print("result:", result)
+        moves = self.env['stock.move.line'].browse([tup[0] for tup in result])
+        return [
+            ('id', 'in', moves.ids),
+        ]
 
     gls_stock_id = fields.Many2one('gls.stock', compute="compute_gls_stock_id", store=True)
-    partner_id = fields.Many2one('res.partner',related='gls_stock_id.partner_id', string='Tedarikçi')
+    partner_id = fields.Many2one('res.partner', related='gls_stock_id.partner_id', string='Tedarikçi')
     product_id = fields.Many2one('product.product', related='gls_stock_id.product_id', string='Ürün')
-    product_qty = fields.Float(related='gls_stock_id.stock_move_id.product_qty',string='Adet')
+    product_qty = fields.Float(related='gls_stock_id.stock_move_id.product_qty', string='Adet')
     line_ids = fields.One2many('weight.measurement.line', 'weight_measurement_id')
     gross_weight = fields.Float()
     net_weight = fields.Float()
-    stock_move_line_id = fields.Many2one('stock.move.line', "Lot Numarası", domain=lambda self: [('location_dest_id', '=', self.env.ref('stock.stock_location_company').id)])
+    stock_move_line_id = fields.Many2one('stock.move.line', "Lot Numarası",
+                                         domain=lambda self: self._get_move_line_domain())
 
     def action_create_pallet_lines(self):
         self.ensure_one()
         if self.gls_stock_id and not self.line_ids:
-            self.line_ids = [(0,0,{'name':f'Palet {i}',}) for i in range(1, int(self.gls_stock_id.stock_move_id.product_qty) + 1)]
+            self.line_ids = [(0, 0, {'name': f'Palet {i}', })
+                             for i in range(1, int(self.gls_stock_id.stock_move_id.product_qty) + 1)]
 
-    
     @api.constrains('line_ids')
     def _constraint_line_ids(self):
         for rec in self:
@@ -29,18 +46,15 @@ class WeightMeasurement(models.Model):
                 rec.gross_weight = sum(rec.line_ids.mapped('value'))
                 rec.net_weight = rec.gross_weight - 30
 
-
-
     @api.depends('stock_move_line_id')
     def compute_gls_stock_id(self):
         for rec in self:
-            rec.gls_stock_id = self.env['gls.stock'].search([('stock_move_id', '=', rec.stock_move_line_id.move_id.id), ('analysis_id.pallet_weight', '=', True)], limit=1).id
-
+            rec.gls_stock_id = self.env['gls.stock'].search(
+                [('stock_move_id', '=', rec.stock_move_line_id.move_id.id), ('analysis_id.pallet_weight', '=', True)], limit=1).id
 
 
 class WeightMeasurementLine(models.Model):
     _name = 'weight.measurement.line'
-
 
     name = fields.Char("No")
     value = fields.Integer("Değer")
